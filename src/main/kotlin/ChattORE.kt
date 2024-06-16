paulwalker-dev/ChattORE

commit 2641a3447cb6d78c4b18a8d63411ccf1cf49db45

Fix periods, commas in URL

src/main/kotlin/ChattORE.kt
@@ -248,7 +248,7 @@ class ChattORE @Inject constructor(val proxy: ProxyServer, val logger: Logger, @
                 this.replace("&k", "")
             }
         val canObfuscate = player?.hasPermission("chattore.chat.obfuscate") ?: false
-        val urlRegex = """<?((http|https)://([\w_-]+(?:\.[\w_-]+)+)([^\s'<>]+)?)>?""".toRegex()
+        val urlRegex = """<?((http|https)://([\w_-]+(?:\.[\w_-]+)+)([^\s',.<>]+)?)>?""".toRegex()
         val parts = urlRegex.split(message)
         val matches = urlRegex.findAll(message).iterator()
         val builder = Component.text()

commit f54f4537a3ac8faf5a966bd1c4e7baa47c0cea23

2641a3447cb6d78c4b18a8d63411ccf1cf49db45 caused more problems than it fixed

src/main/kotlin/ChattORE.kt
@@ -248,7 +248,7 @@ class ChattORE @Inject constructor(val proxy: ProxyServer, val logger: Logger, @
                 this.replace("&k", "")
             }
         val canObfuscate = player?.hasPermission("chattore.chat.obfuscate") ?: false
-        val urlRegex = """<?((http|https)://([\w_-]+(?:\.[\w_-]+)+)([^\s',.<>]+)?)>?""".toRegex()
+        val urlRegex = """<?((http|https)://([\w_-]+(?:\.[\w_-]+)+)([^\s'<>]+)?)>?""".toRegex()
         val parts = urlRegex.split(message)
         val matches = urlRegex.findAll(message).iterator()
         val builder = Component.text()
@@ -261,10 +261,10 @@ class ChattORE @Inject constructor(val proxy: ProxyServer, val logger: Logger, @
                 var name = link.host
                 if (link.file.isNotEmpty()) {
                     val last = link.path.split("/").last()
-                    if (last.contains('.')) {
+                    if (last.contains('.') && !last.endsWith('.') && !last.startsWith('.')) {
                         type = last.split('.').last()
-                        name = if (last.length > 20) {
-                            last.substring(0, 20) + "…." + type
+                        name = if (last.length > 15) {
+                            last.substring(0, 15) + "…." + type
                         } else {
                             last
                         }