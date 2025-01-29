9f812e83bd31fd01c70ae97b64b86b13fb9e7cfa
Nicholas Stonecipher <nickster258@users.noreply.github.com>
2025-01-28 19:32:53 -0500

    Update some build stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    kotlin("jvm") version "2.0.10"
     id("com.gradleup.shadow") version "8.3.0"
-    id("org.jetbrains.kotlin.jvm") version "1.9.22"
     id("org.jetbrains.kotlin.kapt") version "1.9.22"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
@@ -11,7 +11,6 @@ group = ""
 version = "1.1"
 
 repositories {
-    jcenter()
     mavenCentral()
     maven {
         name = "sonatype-oss"
@@ -32,22 +31,22 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation(group = "com.uchuhimo", name = "konf", version = "0.22.1")
+    implementation(group = "com.uchuhimo", name = "konf", version = "1.1.2")
     implementation(group = "net.luckperms", name = "api", version = "5.1")
-    implementation(group = "org.jetbrains.exposed", name = "exposed-core", version = "0.51.1")
-    implementation(group = "org.jetbrains.exposed", name = "exposed-jdbc", version = "0.51.1")
-    implementation(group = "org.jetbrains.exposed", name = "exposed-java-time", version = "0.51.1")
+    implementation(group = "org.jetbrains.exposed", name = "exposed-core", version = "0.58.0")
+    implementation(group = "org.jetbrains.exposed", name = "exposed-jdbc", version = "0.58.0")
+    implementation(group = "org.jetbrains.exposed", name = "exposed-java-time", version = "0.58.0")
     implementation(group = "org.xerial", name = "sqlite-jdbc", version = "3.46.0.0")
     implementation(group = "co.aikar", name = "acf-velocity", version = "0.5.1-SNAPSHOT")
     implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-serialization-json", version = "1.6.0")
     implementation(group = "org.javacord", name = "javacord", version = "3.8.0")
-    implementation(group = "com.velocitypowered", name = "velocity-api", version = "3.2.0-SNAPSHOT")
+    implementation(group = "com.velocitypowered", name = "velocity-api", version = "3.3.0-SNAPSHOT")
     implementation("com.google.code.gson:gson:2.10.1")
-    kapt(group = "com.velocitypowered", name = "velocity-api", version = "3.2.0-SNAPSHOT")
+    kapt(group = "com.velocitypowered", name = "velocity-api", version = "3.3.0-SNAPSHOT")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+    kotlinOptions.jvmTarget = "22"
     kotlinOptions.javaParameters = true
 }
 
