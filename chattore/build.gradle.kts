d0b368002f2761c54ee510561ea6fc6eda4bd0ce
Nicholas Stonecipher <nickster258@users.noreply.github.com>
2025-02-22 14:31:39 -0500

    Reorganize dependencies, use compileOnly where applicable

--- chattore/build.gradle.kts
+++ chattore/build.gradle.kts
@@ -34,16 +34,16 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(group = "com.uchuhimo", name = "konf", version = "1.1.2")
-    implementation(group = "net.luckperms", name = "api", version = "5.1")
+    implementation(group = "com.google.code.gson", name = "gson", version = "2.10.1")
+    implementation(group = "co.aikar", name = "acf-velocity", version = "0.5.1-SNAPSHOT")
+    implementation(group = "org.javacord", name = "javacord", version = "3.8.0")
     implementation(group = "org.jetbrains.exposed", name = "exposed-core", version = "0.58.0")
     implementation(group = "org.jetbrains.exposed", name = "exposed-jdbc", version = "0.58.0")
     implementation(group = "org.jetbrains.exposed", name = "exposed-java-time", version = "0.58.0")
-    implementation(group = "org.xerial", name = "sqlite-jdbc", version = "3.46.0.0")
-    implementation(group = "co.aikar", name = "acf-velocity", version = "0.5.1-SNAPSHOT")
     implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-serialization-json", version = "1.6.0")
-    implementation(group = "org.javacord", name = "javacord", version = "3.8.0")
-    implementation(group = "com.velocitypowered", name = "velocity-api", version = "3.3.0-SNAPSHOT")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation(group = "org.xerial", name = "sqlite-jdbc", version = "3.46.0.0")
+    compileOnly(group = "net.luckperms", name = "api", version = "5.1")
+    compileOnly(group = "com.velocitypowered", name = "velocity-api", version = "3.3.0-SNAPSHOT")
     kapt(group = "com.velocitypowered", name = "velocity-api", version = "3.3.0-SNAPSHOT")
 }
 
